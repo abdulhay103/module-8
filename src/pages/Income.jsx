21f2e6fe11e773bc067e4485d8792e81aff4ad1a
abdulhay103 <mdabdulhay95@gmail.comm>
2023-07-24 23:58:40 +0600

    working

--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import IncomeFile from "../components/IncomeFile";
 
 const Income = () => {
   return (
     <Layout>
-      <h1>Income Page</h1>;
+      <IncomeFile />
     </Layout>
   );
 };
